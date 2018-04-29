ea60b17f99bce983af5d59354d755bebff52d09b
DESKTOP-DRQ7MTF\Jed Diaz <jemjem_zms@hotmail.com>
2018-04-29 20:47:08 +1200

    Adding the config file

---
+++ _ss_environment.php
@@ -0,0 +1,10 @@
+<?php
+
+define('SS_DATABASE_SERVER','localhost');
+define('SS_DATABASE_USERNAME','root');
+define('SS_DATABASE_PASSWORD','');
+define('SS_DATABASE_NAME','carservice_db');
+define('SS_ENVIRONMENT_TYPE','dev');
+define('SS_DATABASE_CHOOSE_NAME', true);
+define('SS_DEFAULT_ADMIN_USERNAME','admin');
+define('SS_DEFAULT_ADMIN_PASSWORD','ch@ng3iT');
